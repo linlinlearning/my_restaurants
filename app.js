bd49f8f133c03969c257a2029cee28648f22aa17
linlinlearning <linlinlearning@gmail.com>
2022-06-30 22:25:22 +0800

    feat: add seeder

--- app.js
+++ app.js
@@ -2,12 +2,13 @@
 const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
-
+const bodyParser = require('body-parser')
 // Require mongoose
 const mongoose = require('mongoose')
 
 // mongoose connection
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
 
 // Get mongoose connection status
 const db = mongoose.connection
@@ -22,7 +23,8 @@ db.once('open', () => {
 
 
 // Require restaurant list
-const restaurantList = require('./restaurant.json')
+//const restaurantList = require('./restaurant.json')
+const Restaurant = require('./models/restaurant') // 載入 Restaurant model
 
 // Define port
 const port = 3000
@@ -34,10 +36,23 @@ app.set('view engine', 'handlebars')
 // Set static files
 app.use(express.static('public'))
 
-// Define route for index page
+/* Define route for index page
 app.get('/', (req, res) => {
   res.render('index', { restaurants: restaurantList.results })
 })
+*/
+
+// Define route for index page
+app.get('/', (req, res) => {
+  Restaurant.find()
+    .lean()
+    .then(restaurants => {
+      res.render('index', { restaurants })
+    })
+    .catch(error => console.log(error))
+})
+
+
 
 // Define route for show page
 app.get('/restaurants/:restaurant_id', (req, res) => {
--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,21 +1,27 @@
 const mongoose = require('mongoose')
 const Restaurant = require('../restaurant') // 載入 restaurant model
-
-const restaurantList = require("../../restaurant.json").results
+const Seeder = require('../restaurant.json')
+const seederData = Seeder.results
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-
 const db = mongoose.connection
 db.on('error', () => {
   console.log('mongodb error!')
 })
 db.once('open', () => {
   console.log('mongodb connected!')
-  Restaurant.create(restaurantList)
-    .then(() => {
-      console.log("restaurantSeeder done!")
-      db.close()
+  for (let i = 0; i < seederData.length; i++) {
+    Restaurant.create({
+      id: seederData[i].id,
+      name: seederData[i].name,
+      name_en: seederData[i].name_en,
+      category: seederData[i].category,
+      image: seederData[i].image,
+      location: seederData[i].location,
+      phone: seederData[i].phone,
+      google_map: seederData[i].google_map,
+      rating: seederData[i].rating,
+      description: seederData[i].description
     })
-    .catch(err => console.log(err))
-
+  }
 })
